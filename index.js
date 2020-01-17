2ae52c71f68d293df84d0d1e986062a7132ef8e6
vierge <marancholia@gmail.com>
2020-01-17 13:29:08 -0500

    completed refactor!

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-// Arrays to keep track of each task's state
+
 function newTask (title, description) {
   const task = {
     title,
     description,
-    complete: false
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? "" : " not"} been completed${this.complete ? "!!! ✨" : "... 🤔"}`);
+    },
+
+    markComplete: function () {
+      this.complete = true;
+    }
   };
   return task;
-}
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(task) {
-  task.complete = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? "" : " not"} been completed${task.complete ? "!!! ✨" : "... 🤔"}`);
 }
 
 
@@ -25,6 +23,6 @@ const task1 = newTask("Clean Cat Litter", "come on, you know how to scoop");
 const task2 = newTask("Do Laundry", "💦");
 const tasks = [task1, task2];
 
-logTaskState(task1);
-completeTask(task1);
-logTaskState(task1);
+task1.logState();
+task1.markComplete();
+task1.logState();
